823b039f66298d5ef9a6ce74492e1d4b225b7543
wook95 <uclee95@gmail.com>
2022-03-07 22:39:51 +0900

    config: 초기 세팅

---
+++ screens/RootStack.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+const RootStack = () => {
+  return <Stack.Navigator>{/*화면 추가 예정 */}</Stack.Navigator>;
+};
+
+export default RootStack;
