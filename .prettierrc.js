55cbeb5f39fd24a7505cc28e0bc6136b19e950c0
wook95 <uclee95@gmail.com>
2022-03-08 23:43:12 +0900

    config: prettier 룰 수정

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,13 @@
 module.exports = {
-  bracketSpacing: false,
+  semi: true,
+  bracketSpacing: true,
+  bracketSameLine: true,
   jsxBracketSameLine: true,
   singleQuote: true,
   trailingComma: 'all',
   arrowParens: 'avoid',
+  tabWidth: 2,
+  printWidth: 80,
+  endOfLine: 'lf',
+  jsxSingleQuote: true,
 };
